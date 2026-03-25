dacfc53ac9d51264bca87caaf1c323836b8083f8
cristianramirez414 <cristian.ramirez414@pascualbravo.edu.co>
2026-03-25 13:35:07 -0500

    se realizo el registro para la pagina del login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { useUserStore } from './context/useUserStore';
 import Login from './pages/Home/Login';
 import Home from './pages/Home/Home';
+import Register from './pages/Home/registro'; 
 
 function App() {
   const isAuthenticated = useUserStore((state) => state.isAuthenticated);
@@ -9,7 +10,13 @@ function App() {
   return (
     <Router>
       <Routes>
+        {}
         <Route path="/" element={<Login />} />
+
+        {}
+        <Route path="/register" element={<Register />} />
+
+        {}
         <Route 
           path="/home" 
           element={isAuthenticated ? <Home /> : <Navigate to="/" />} 
